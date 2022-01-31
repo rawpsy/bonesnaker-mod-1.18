956b2ea258372d33aa05bc8b79fc99d6be3fc764
rawpsy <raxxtq@gmail.com>
2022-01-31 15:31:02 +1100

    Initial commit

---
+++ src/main/java/au/com/snakerbone/SnakerBoneMod.java
@@ -0,0 +1,16 @@
+package au.com.snakerbone;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SnakerBoneMod implements ModInitializer {
+	public static final String MOD_ID = "snakerbone";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/au/com/snakerbone/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package au.com.snakerbone.mixin;
+
+import au.com.snakerbone.SnakerBoneMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		SnakerBoneMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
